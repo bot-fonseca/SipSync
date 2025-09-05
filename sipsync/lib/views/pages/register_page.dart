339d052c0ae0b345d6026f3e50ee55c61d8a806d
Andre <andrefilipe0205@gmail.com>
2025-09-05 22:51:52 +0100

    Register Page modificationss

--- sipsync/lib/views/pages/register_page.dart
+++ sipsync/lib/views/pages/register_page.dart
@@ -5,8 +5,6 @@ import 'package:sipsync/views/widgets/hero_widget.dart';
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +15,83 @@ class RegisterPage extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(width: 400, child: HeroWidget(title: 'Create account')),
-
+              SizedBox(height: 12),
+              TextField(
+                // controller: controllerEmail,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Name/username',
+                ),
+                // onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                // controller: controllerEmail,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Insert Age',
+                ),
+                // onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                // controller: controllerEmail,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Insert your height',
+                ),
+                // onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                // controller: controllerEmail,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Insert your weight',
+                ),
+                // onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                // controller: controllerEmail,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Insert your Email',
+                ),
+                // onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                // controller: controllerEmail,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Insert your Password',
+                ),
+                // onEditingComplete: () => setState(() {}),
+              ),
+              SizedBox(height: 12),
               FilledButton(
                 onPressed: () {
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return LoginPage(title: 'Login');
-                        },
-                      ),
-                    );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginPage(title: 'Login');
+                      },
+                    ),
+                  );
                 },
                 style: ElevatedButton.styleFrom(
                   minimumSize: Size(double.infinity, 50),
